a940542e409f383a7ad8b33754ff57cd5f2b4d9c
Scott Hoisington <hoyzington@gmail.com>
2019-11-03 22:48:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,19 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :name => "Lord Montague",
-        :age => "53"
-        },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
    }, 
    :capulet => {
@@ -26,8 +16,9 @@ def third_challenge
         :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
-        },
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"        },
       :heroine_friends => []
    }
   }
