5ed24b930183b539957c78216a7c89857a246a27
Scott Hoisington <hoyzington@gmail.com>
2019-11-03 22:40:41 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,8 @@ def third_challenge
         :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name => "Lady Capulet",
+        :age => "51"
         },
       :heroine => {
         #your key/value pairs here
